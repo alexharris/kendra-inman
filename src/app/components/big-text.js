9885ce049af79e8e0a28b2d048172564e70cf467
alex harris <alexharris6@gmail.com>
2025-07-28 17:27:07 -0700

    decrease mobile big text

--- src/app/components/big-text.js
+++ src/app/components/big-text.js
@@ -1,6 +1,6 @@
 export default function BigText({ children, className = "" }) {
   return (
-    <span className={`block text-9xl w-1/2 ${className}`}>
+    <span className={`block text-3xl md:text-6xl lg:text-9xl w-1/2 ${className}`}>
       {children}
     </span>
   )
