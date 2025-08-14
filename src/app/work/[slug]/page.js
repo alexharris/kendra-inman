29f21a479fe188d1083291070c0cfd65f1da160a
alex harris <alexharris6@gmail.com>
2025-08-14 13:09:24 -0700

    start roughing in project page

--- src/app/work/[slug]/page.js
+++ src/app/work/[slug]/page.js
@@ -5,16 +5,17 @@ export default function Page() {
     <div className="project-container bg-red flex flex-col text-white px-4">
       <div id="project-overview" className="flex flex-col">
         <div id="project-slideshow" className="w-full flex-1 bg-gray-200"></div>
-        <div id="project-intro" className="flex flex-row items-center justify-between h-32 py-6 flex-shrink-0">
+        <div id="project-intro" className="bg-red z-20 flex flex-row items-center justify-between h-32 py-6 flex-shrink-0">
           <h1 id="project-title" className="serif-header">ByHeart</h1>
           <div id="project-tagline-1" className="uppercase flex flex-row items-center gap-2">Meet the Mother of All Formulas <span className="font-mono text-xs">12</span></div>
           <div id="project-information-below" className="font-mono uppercase text-xs">Project Information Below</div>
         </div>
       </div>
 
-      <div> 
+      <div className="bg-red flex flex-col gap-10 pb-16">
         <div id="project-tagline-2" className="uppercase flex flex-row items-center gap-2">Meet the Mother of All Formulas <span className="font-mono text-xs">12</span></div>
-        <p className="big-paragraph">Branding, illustration, art direction and website for the 2016 launch of Billie — the first progressive body care company of its kind lorem ipsum etc.</p>
+        <p className="big-paragraph font-serif w-2/3">Branding, illustration, art direction and website for the 2016 launch of Billie — the first progressive body care company of its kind lorem ipsum etc.</p>
+        <div className="h-96"></div>
       </div>
 
     </div>
