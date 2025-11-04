a4e98dfa377f68cf4dcce9bf1c825ff5a1e028a5
Alex Harris <alexharris6@gmail.com>
2025-11-04 13:14:27 -0800

    switch to big text clamp

--- src/app/components/big-text.js
+++ src/app/components/big-text.js
@@ -1,6 +1,6 @@
 export default function BigText({ children, className = "" }) {
   return (
-    <span className={`big-text w-full xl:w-9/12 font-serif block text-[70px] md:text-[120px] lg:text-[160px] leading-none ${className}`}>
+    <span className={`big-text w-full sm:w-3/4 lg:w-7/12 font-serif block leading-none ${className}`}>
       {children}
     </span>
   )
