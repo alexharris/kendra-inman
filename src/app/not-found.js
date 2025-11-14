1255e5272142c69e1778673a5961dea36aea0e6b
Alex Harris <alexharris6@gmail.com>
2025-11-13 20:15:58 -0800

    initial 404 page

---
+++ src/app/not-found.js
@@ -0,0 +1,44 @@
+'use client';
+
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { setPageColors } from '../utils/pageColors';
+import BigText from "./components/big-text";
+
+
+export default function NotFound() {
+  // Set page colors to match the brand aesthetic
+  useEffect(() => {
+    setPageColors({
+      background: 'bg-black',
+      text: 'text-beige',
+      fill: 'fill-beige',
+      applyToFooter: true,
+    });
+  }, []);
+
+  return (
+    <div className="min-h-[70vh] flex flex-col items-center justify-center p-4 md:p-12 bg-black text-beige">
+      <div className="max-w-[1400px] mx-auto text-center">
+        {/* Large 404 */}
+            <BigText 
+              className=""
+            >
+              God is in the details.
+            </BigText>        
+        
+        {/* Error message */}
+        <h1 className="uppercase mt-8 mb-8">
+          MIES VAN DER WOHE
+        </h1>
+        
+        {/* Description */}
+        <p className="text-lg md:text-4xl max-w-2xl mx-auto">
+          Even the 404. But you came for <a className="underline" href="/">this</a>.
+        </p>
+        
+    
+      </div>
+    </div>
+  );
+}
